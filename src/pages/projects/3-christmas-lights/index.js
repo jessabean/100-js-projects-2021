45362ca6d47bb494ffa75c3c55374258c651a5dc
jessabean <jessica.eldredge@splice.com>
2021-05-21 13:55:21 -0400

    Convert lights class to function

--- src/pages/projects/3-christmas-lights/index.js
+++ src/pages/projects/3-christmas-lights/index.js
@@ -1,44 +1,35 @@
-import * as React from "react"
+import React, { useState, useRef } from "react"
 import Light from './light'
 
 import Layout from "../../../components/layout"
 import Seo from "../../../components/seo"
 import * as pageStyles from "./christmas-lights.module.css"
 
-export default class ChristmasLights extends React.Component {
-  constructor(props) {
-    super(props);
-    this.stringOfLights = 7;
+function ChristmasLights() {
+  const [currentKey, setState] = useState(null);
+  const stringOfLights = 7;
+  const lightRefs = useRef([...new Array(stringOfLights)].map(() => React.createRef()));
 
-    for (let i=1; i <= this.stringOfLights; i++) {
-      this['lightRef'+i] = React.createRef();
-    }
-
-    this.state = {
-      currentKey: null,
-    };
+  const foo = () => {
+    console.log('foo');
   }
 
-  render () {
-    const lights = []
-
-    for (let i=1; i <= this.stringOfLights; i++) {
-      lights.push(<Light key={i} ref={this['lightRef'+i]} />)
-    }
-
-    return (
-      <Layout>
-        <Seo title="Christmas Lights Simulator" />
-        <section className="project">
-          <header className="project-header">
-            <h1 className="project-headline">Christmas lights</h1>
-            <p className="project-description">Click a light to change its color. Click the "Start" button to see the lights in action!</p>
-          </header>
-          <div className={pageStyles.['preview__wrapper']}>
-            { lights }
-          </div>
-        </section>
-      </Layout>
-    )
-  }
+  return (
+    <Layout>
+      <Seo title="Christmas Lights Simulator" />
+      <section className="project">
+        <header className="project-header">
+          <h1 className="project-headline">Christmas lights</h1>
+          <p className="project-description">Click a light to change its color. Click the "Start" button to see the lights in action!</p>
+        </header>
+        <div className={pageStyles.['preview__wrapper']}>
+          {lightRefs.current.map((el, i) =>
+            <Light key={i} ref={el => (lightRefs.current[i] = el)} onClick="foo()" />
+          )}
+        </div>
+      </section>
+    </Layout>
+  )
 }
+
+export default ChristmasLights;
